c551ec569dd272c2a86ecd30b12a7e72d04fa70b
Akbar Rouzi <seagull826@gmail.com>
2024-08-06 16:33:13 -0400

    display product list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import './App.css';
+
 function App() {
+
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('https://dummyjson.com/products?limit=100');
+      const data = await res.json();
+      console.log('data: ', data);
+
+      if (data && data.products) {
+        setProducts(data.products);
+      }
+
+    })();
+  }, []);
+
   return (
     <div className="App">
-       Learn React
+      {products.length > 0 &&
+        <div className="products">
+          {products.map(product => (
+            <div className="product" key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <span>{product.title}</span>
+            </div>
+          ))}
+        </div>
+      }
     </div>
   );
 }
