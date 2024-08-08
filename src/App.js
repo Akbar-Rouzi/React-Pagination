8bbd33d25661f4c41ae9bba5d279c6196e9c1979
Akbar Rouzi <seagull826@gmail.com>
2024-08-08 15:05:29 -0400

    Add pagination button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 function App() {
 
   const [products, setProducts] = useState([]);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalPage, setTotalPage] = useState(0);
+
   useEffect(() => {
     (async () => {
-      const res = await fetch('https://dummyjson.com/products?limit=100');
+      const res = await fetch('https://dummyjson.com/products?limit=30');
       const data = await res.json();
       console.log('data: ', data);
 
       if (data && data.products) {
         setProducts(data.products);
+        setTotalPage(Math.ceil(100 / pageSize));
       }
 
     })();
@@ -20,13 +24,24 @@ function App() {
   return (
     <div className="App">
       {products.length > 0 &&
-        <div className="products">
-          {products.map(product => (
-            <div className="product" key={product.id}>
-              <img src={product.thumbnail} alt={product.title} />
-              <span>{product.title}</span>
-            </div>
-          ))}
+        <div className="content">
+          <div className="products">
+            {products.map(product => (
+              <div className="product" key={product.id}>
+                <img src={product.thumbnail} alt={product.title} />
+                <span>{product.title}</span>
+              </div>
+            ))}
+          </div>
+          <div className="pagination">
+            <span>◀</span>
+            {
+              Array.from({ length: totalPage }, (_, i) => (
+                <span key={i + 1}>{i + 1}</span>
+              ))
+            }
+            <span>▶</span>
+          </div>
         </div>
       }
     </div>
